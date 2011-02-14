49d0b8f5602e0bb46cb2b44ac6dd1b6fa30f813c
Luis Parravicini <lparravi@gmail.com>
2011-02-14 11:38:33 -0300

    typo en nombre del template

--- includes/footer.php
+++ includes/footer.php
@@ -2,7 +2,7 @@
 	<div id="bottominfo">
 	<div id="footer">
 		<div class="container">
-			<a id="designby" href="http://opensourcetemplates.org/preview/eco-business-website-templates/" alt='Basado en el template eeco_business'>Basado en el template eco_business</a>
+			<a id="designby" href="http://opensourcetemplates.org/preview/eco-business-website-templates/" alt='Basado en el template eco_business'>Basado en el template eco_business</a>
 		</div>
 	</div>
 </body>
